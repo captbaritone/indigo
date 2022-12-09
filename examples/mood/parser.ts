8bb6fb7f1e726f2322a33960e1ae928a4d9e589e
Jordan Eldredge <jordan@jordaneldredge.com>
2022-12-08 18:41:28 -0800

    Throw diagnostic on parse error

--- examples/mood/parser.ts
+++ examples/mood/parser.ts
@@ -1,7 +1,23 @@
 import peg from "pegjs";
 import fs from "fs";
+import { AstNode } from "./ast";
+import DiagnosticError, { annotate } from "./DiagnosticError";
 
 const GRAMMAR = fs.readFileSync("./examples/mood/mood.pegjs", "utf-8");
 
 const parser = peg.generate(GRAMMAR);
-export default parser;
+export default {
+  parse(code: string): AstNode {
+    try {
+      return parser.parse(code);
+    } catch (e) {
+      if (e instanceof parser.SyntaxError) {
+        throw new DiagnosticError(
+          e.message,
+          annotate(e.location, "Syntax error"),
+        );
+      }
+      throw e;
+    }
+  },
+};
